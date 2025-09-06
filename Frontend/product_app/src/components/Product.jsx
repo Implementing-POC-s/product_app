39cf2b3186021271d0a54460d3015a5efb786933
Sahil-quabyt <sahil.kawatra1@outlook.com>
2025-09-06 22:27:24 +0530

    added frontend

---
+++ Frontend/product_app/src/components/Product.jsx
@@ -0,0 +1,45 @@
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
+function Product() {
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get("https://localhost:7212/api/ProductAPI", {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    }
+                });
+                const data = response.data;
+                console.log(response);
+                setProducts(data);
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+        fetchProducts();
+      }, [])//dependency array
+    return (
+      
+      <>
+          <div>
+              All products
+          </div>
+          <div>
+              {products.map((product) => (
+                  <div id={product.id}>
+                      <span>{product.name}</span>-<span>{product.price}</span>
+                  </div>
+              ))}
+              </div>
+
+              
+          
+              
+    </>
+  )
+}
+
+export default Product
